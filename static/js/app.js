fee38c0e4a53446166d8c5b6526455911dada719
Deborah <deborahigbalajobi@yahoo.com>
2020-05-11 19:13:19 -0400

    final code

--- static/js/app.js
+++ static/js/app.js
@@ -1,46 +1,49 @@
-// from data.js
-var tableData = data;
+//from data.js
+//var tableData = data;
 var tbody = d3.select("tbody");
 // YOUR CODE HERE!
-data.forEach((tableData) => {
+//data.forEach((tableData) => {
+    //var row = tbody.append("tr");
+    //Object.entries(tableData).forEach(([key, value]) => {
+      //var cell = row.append("td");
+      //cell.text(value);
+    //})
+//});
+
+//tbody = d3.select("tbody")
+
+function printdata(tableData){ 
+  tbody.html("")
+  tableData.forEach((datarow)=>{
     var row = tbody.append("tr");
-    Object.entries(tableData).forEach(([key, value]) => {
+    Object.entries(datarow).forEach(([key, value]) => {
       var cell = row.append("td");
       cell.text(value);
     })
-});
+  });
+}
+printdata(data)
 
-tbody = d3.select("tbody")
-
-function displayData(data){ 
-  tbody.text("")
-  data.forEach(function(sighting){
-  new_tr = tbody.append("tr")
-  Object.entries(sighting).forEach(function([key, value]){
-      new_td = new_tr.append("td").text(value)	
-  })
-})}
-
-displayData(tableData)
 
 // Select button on form for date filtering
 var button = d3.select("#filter-btn");
 // Select form used for entering date to filter
-var form = d3.select("#form");
+//var form = d3.select("#form");
 // Create event handlers for clicking the button to filter by date
 // or by pressing enter
-button.on("click", clickSelect);
-form.on("submit", clickSelect);
+button.on("click", runEnter);
+//form.on("submit", clickSelect);
 // Create funcion to run for both clicking the button and pressing enter
-function clickSelect() {
+function runEnter() {
   // Prevent the page from refreshing
-  d3.event.preventDefault();
+  //d3.event.preventDefault();
   // Define variable for input date and select id that stores the date
-  //var inputDate = d3.select("#datetime");
+  var input = d3.select("#datetime").property("value");
   // Define variable for value property of input date
-  console.log(button.property("value"));
-  var new_table = tableData.filter(sighting => sighting.datetime===button.property("inputValue"))
-  displayData(new_table);
-}
+  console.log(input);
+  var filterData = data.filter(row => row.datetime===input);
+  console.log(filterData);
+  printdata(filterData)
+};
 
-button.on("change", clickSelect)
+//button.on("change", clickSelect)
